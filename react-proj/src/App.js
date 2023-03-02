31b7b727c34702e99550b94a2046d6de0cf0318c
Raphaela664 <mahoraphy02@gmail.com>
2023-03-02 14:12:39 +0200

    issues

---
+++ react-proj/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BlogList from './blogList';
+function App() {
+  return (
+    <div>
+      <BlogList />
+    </div>
+  )
+    
+}
+
+export default App;
---
+++ react-proj/src/blogList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BlogList() {
+  return (
+    <div>
+      
+      <h1>Latest From The Blog</h1>
+      
+    </div>
+  )
+}
